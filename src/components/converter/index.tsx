8f42996354086d2474a248ada17a9fb71ed33554
Carla Paiva <carla.paiva05@hotmail.com>
2024-01-06 18:01:43 -0300

    feat: basic stuff

---
+++ src/components/converter/index.tsx
@@ -0,0 +1,7 @@
+function Converter(): JSX.Element {
+    return (
+        <div></div>
+    )
+}
+
+export default Converter
---
+++ src/components/header/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd';
+const Title  = Typography.Title
+
+function Header(): JSX.Element {
+    return (
+      <Typography>
+        <Title>MediaTransmute</Title>
+      </Typography>
+    )
+  }
+  
+  export default Header
+  
