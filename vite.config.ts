20b066007cd943821b149d89afb59a096b11ece0
Carla Paiva <carla.paiva05@hotmail.com>
2024-01-07 00:15:25 -0300

    feat: suppor for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://carlapaiva.github.io/media-transmute/',
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
